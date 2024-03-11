60e04a6fe912f541508f6a9cfc6d51a78bbad56b
Alexander Papchikhin <aleshmandr@gmail.com>
2024-03-11 18:34:21 +0100

    Dropdown feature

---
+++ Assets/Plugins/StompyRobot/SRDebugger/Scripts/SRDropDownData.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace SRDebugger {
+    public class SRDropDownData {
+
+        public event Action<int> SelectionChanged; 
+        public readonly object[] Options;
+        private int _selectionIndex;
+
+        public int SelectionIndex
+        {
+            get => _selectionIndex;
+            set
+            {
+                if(_selectionIndex == value)
+                {
+                    return;
+                }
+                _selectionIndex = value;
+                SelectionChanged?.Invoke(_selectionIndex);
+            }
+        }
+
+        public SRDropDownData(int selectionIndex, object[] options)
+        {
+            _selectionIndex = selectionIndex;
+            Options = options;
+        }
+    }
+}
---
+++ Assets/Plugins/StompyRobot/SRDebugger/Scripts/UI/Controls/Data/DropDownControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+using SRF;
+using UnityEngine.UI;
+
+namespace SRDebugger.UI.Controls.Data
+{
+    public class DropDownControl : DataBoundControl
+    {
+        [RequiredField] public Text Title;
+        [RequiredField] public Dropdown Dropdown;
+        private SRDropDownData _dropdownData;
+
+        protected override void Start()
+        {
+            base.Start();
+            Dropdown.onValueChanged.AddListener(OnValueChanged);
+        }
+
+        protected override void OnDestroy()
+        {
+            Dropdown.onValueChanged.RemoveAllListeners();
+            base.OnDestroy();
+        }
+
+        private void OnValueChanged(int index)
+        {
+            _dropdownData.SelectionIndex = index;
+        }
+
+        protected override void OnBind(string propertyName, Type t)
+        {
+            Debug.Log($"On Bind property={propertyName}");
+            base.OnBind(propertyName, t);
+            Title.text = propertyName;
+        }
+
+        protected override void OnValueUpdated(object newValue)
+        {
+            _dropdownData = newValue as SRDropDownData;
+            if(_dropdownData == null)
+            {
+                Dropdown.ClearOptions();
+                return;
+            }
+            List<string> options = new List<string>();
+            foreach(var valueOption in _dropdownData.Options)
+            {
+                options.Add(valueOption.ToString());
+            }
+            Dropdown.AddOptions(options);
+            Dropdown.SetValueWithoutNotify(_dropdownData.SelectionIndex);
+        }
+
+        public override bool CanBind(Type type, bool isReadOnly)
+        {
+            return type == typeof (SRDropDownData);
+        }
+    }
+}
